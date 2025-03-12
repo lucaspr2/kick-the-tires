3f26a591131eb3eaf3316fe99955ed4d6c512b96
lucaspr2 <lucasp@writechoice.io>
2025-03-12 15:52:39 -0300

    Update main.tf

--- staging/compute/main.tf
+++ staging/compute/main.tf
@@ -2,7 +2,7 @@ module "staging_compute_instance" {
   source = "../../modules/compute"
 
   instance_count = 0
-  instance_name  = "staging-instance-modified"
+  instance_name  = "staging-instance-modified-1"
   instance_zone  = "us-central1-a"
   boot_disk_image = "debian-cloud/debian-11"
   network_name    = "default"
